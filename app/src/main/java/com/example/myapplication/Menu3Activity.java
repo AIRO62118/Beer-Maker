5b28b3750895b4bf86ee8b3376a003cac141a9d3
eleve <eleve@core>
2021-10-13 11:09:37 +0200

    Probleme RecyclerView

--- app/src/main/java/com/example/myapplication/Menu3Activity.java
+++ app/src/main/java/com/example/myapplication/Menu3Activity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.example.myapplication.RecyclerView.MyAdapter;
 
@@ -16,6 +17,7 @@ public class Menu3Activity extends AppCompatActivity {
     //Attributs
     private RecyclerView listeBdd;
     private AccesLocal accesLocal;
+    TextView nbrRecette;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +25,19 @@ public class Menu3Activity extends AppCompatActivity {
         setContentView(R.layout.activity_menu3);
 
     init();
+
+        ArrayList<Recette> listeRecette = accesLocal.recupEtoile();
+        Log.d("Pourquoi ?", String.valueOf(listeRecette.size()));
+        listeBdd.setLayoutManager(new LinearLayoutManager(this));
+        listeBdd.setAdapter(new MyAdapter(listeRecette));
+
     }
 
     public void init(){
         listeBdd = findViewById(R.id.listeRecette);
         accesLocal = new AccesLocal(this);
+
+        nbrRecette = findViewById(R.id.nbrRecette);
     }
 
 
@@ -36,9 +46,5 @@ public class Menu3Activity extends AppCompatActivity {
     public void onResume(){
         Log.d("MonLog", "OnResume Start");
         super.onResume();
-
-        ArrayList<Recette> listeRecette = accesLocal.recupEtoile();
-        listeBdd.setLayoutManager(new LinearLayoutManager(this));
-        listeBdd.setAdapter(new MyAdapter(listeRecette));
     }
 }
